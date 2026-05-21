34cef37dde8a1cd9267a71bc331136813dbfb383
imduchuyyy <duchuy.124dk@gmail.com>
2026-05-21 15:20:23 +0700

    refactor: clean code

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,5 @@
 export const VERSION = "0.1.0";
 export const API_ENDPOINT = process.env.ASM_API_ENDPOINT || "https://asmai.xyz/api";
+export const AGENT_FILE_LOCATION = {
+  opencode: ".opencode/agent"
+}
--- handlers/setup.handler.ts
+++ handlers/setup.handler.ts
@@ -1,6 +1,4 @@
-const agentFileLocation = {
-  opencode: ".opencode/agent"
-}
+import { AGENT_FILE_LOCATION } from "../constants";
 
 // use bun api to check if agent file is exit or not
 const checkAgentFile = async (fileName: string) => {
@@ -14,7 +12,7 @@ const checkAgentFile = async (fileName: string) => {
 }
 
 export const setupHandler = async () => {
-  for (const [source, path] of Object.entries(agentFileLocation)) {
+  for (const [source, path] of Object.entries(AGENT_FILE_LOCATION)) {
     const exists = await checkAgentFile(path);
     if (!exists) {
       console.log(`Agent file not found at ${path} for source ${source}. Please create it before running the agent.`);
